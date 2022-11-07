027f17f283646b87d7611000c740add41ebbdbbd
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-07 08:55:51 -0500

    Trying timeSlots as a nested Object instead of array

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,31 +8,84 @@ const currentTime = moment(currentTimeDisplay, "hA"); // Get Current Time to 'fl
 init();
 
 function init() {
-  const timeSlots = [
-    "9AM",
-    "10AM",
-    "11AM",
-    "12PM",
-    "1PM",
-    "2PM",
-    "3PM",
-    "4PM",
-    "5PM"
-  ];
+  
+  const timeSlots = {
+    NineAM: {time:"9AM", color: "white", savedEvent: "none"},
+    TenAM:{time:"10AM", color: "white", savedEvent: "none"},
+    ElevenAM:{time:"11AM", color: "white", savedEvent: "none"},
+    NoonPM:{time:"12PM", color: "white", savedEvent: "none"},
+    OnePM:{time:"1PM", color: "white", savedEvent: "none"},
+    TwoPM:{time:"2PM", color: "white", savedEvent: "none"},
+    ThreePM:{time:"3PM", color: "white", savedEvent: "none"},
+    FourPM:{time:"4PM", color: "white", savedEvent: "none"},
+    FivePM:{time:"5PM", color: "white", savedEvent: "none"},
+  };
 
   // Display the current time and date in the jumbo-tron
   currentDayDisplay.text(currentDay);
 
-  console.log(currentTimeDisplay);
+  //console.log(currentTime);
 
-  // Check each time slot to compare if that timeSlot is before, now or after the current time. (in AM/PM format)
-  for (i in timeSlots) {
-    if (moment(timeSlots[i], "hA").isSame(currentTime)) {
-      console.log("same time");
-    } else if (moment(timeSlots[i], "hA").isBefore(currentTime)) {
-      console.log("before time");
-    } else if (moment(timeSlots[i], "hA").isAfter(currentTime)) {
-      console.log("after time");
-    }
-  }
+  
+  // Retrieves innerKey/value pairs of the timeSlots object (Nested ForEach)
+  Object.entries(timeSlots).forEach(([key, val]) => {
+    
+    let hourBlock = (`<p value=${key} style="background-color: ${val.color}">${val.time}`);
+
+    //hourBlock.val(`${val.color}`);
+
+    //console.log(`key: ${key} val: ${val}`);
+
+
+    //Object.entries(val).forEach(([innerKey, innerVal]) => {
+      //console.log(`innerKey: ${innerKey} innerVal: ${innerVal}`);
+      //let innerBlock = (`<p class='innerBlock'>${innerVal}`);
+      
+      //timeBlocksContainer.append(innerBlock);
+      //console.log(`key: ${key} val: ${val.time}`);
+      if (moment(val.time, "hA").isSame(currentTime)) {
+        console.log("same time");
+        hourBlock.addClass("present");
+        timeBlocksContainer.append(hourBlock);
+        //innerKey.color = "red";
+        //innerBlock.css("background-color", "red");
+        //color = red;
+      }
+      else if (moment(val.time, "hA").isBefore(currentTime)) {
+        console.log("before time");
+        hourBlock.addClass("past");
+        timeBlocksContainer.append(hourBlock);
+        //innerVal = "grey";
+        //hourBlock.css("background-color", "grey");
+      } 
+      else if (moment(val.time, "hA").isAfter(currentTime)) {
+        console.log("after time");
+        hourBlock.addClass("future");
+        timeBlocksContainer.append(hourBlock);
+        //innerVal = "green";
+        //hourBlock.css("background-color", "green");
+      }
+
+
+      // if (moment(innerVal, "hA").isSame(currentTime)) {
+      //   console.log("same time");
+      //   innerKey.color = "red";
+      //   //hourBlock.css("background-color", "red");
+      //   //color = red;
+      // }
+      // else if (moment(innerVal, "hA").isBefore(currentTime)) {
+      //   console.log("before time");
+      //   innerVal = "grey";
+      //   //hourBlock.css("background-color", "grey");
+      // } 
+      // else if (moment(innerVal, "hA").isAfter(currentTime)) {
+      //   console.log("after time");
+      //   innerVal = "green";
+      //   //hourBlock.css("background-color", "green");
+      // }
+      
+    });
+    
+  //});
+  
 };
