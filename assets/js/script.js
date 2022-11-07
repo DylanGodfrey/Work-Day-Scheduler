c4288035f10dbffd3b57ccd188b04b72f5ffb071
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-07 07:13:54 -0500

    Created an array to store business hours, for loop through it to determine relative time

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,34 @@ const currentDay = moment().format("dddd, MMMM Do, YYYY"); // Get Current Date
 const currentTimeDisplay = moment().format("hA"); // Get Current Time in AM/PM
 const currentTime = moment(currentTimeDisplay, "hA"); // Get Current Time to 'floored' (rounded down) hour
 
-
 init();
 
 function init() {
+  const timeSlots = [
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM"
+  ];
 
-  // display current date in the header
+  // Display the current time and date in the jumbo-tron
   currentDayDisplay.text(currentDay);
-}
+
+  console.log(currentTimeDisplay);
+
+  // Check each time slot to compare if that timeSlot is before, now or after the current time. (in AM/PM format)
+  for (i in timeSlots) {
+    if (moment(timeSlots[i], "hA").isSame(currentTime)) {
+      console.log("same time");
+    } else if (moment(timeSlots[i], "hA").isBefore(currentTime)) {
+      console.log("before time");
+    } else if (moment(timeSlots[i], "hA").isAfter(currentTime)) {
+      console.log("after time");
+    }
+  }
+};
