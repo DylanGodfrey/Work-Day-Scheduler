f4e06964edb32396a80a15485fcf2d6d41ffa797
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-09 06:10:48 -0500

    Can now save events to local storage and get them from local storage on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,25 @@ const currentTimeDisplay = moment().format("hA"); // Get Current Time in AM/PM
 const currentTime = moment(currentTimeDisplay, "hA"); // Get Current Time to 'floored' (rounded down) hour
 
 
-const timeSlots = [
-  {time:"9AM", relative: "", savedEvent: "No saved event!"},
-  {time:"10AM", relative: "", savedEvent: "No saved event!"},
-  {time:"11AM", relative: "", savedEvent: "No saved event!"},
-  {time:"12PM", relative: "", savedEvent: "No saved event!"},
-  {time:"1PM", relative: "", savedEvent: "No saved event!"},
-  {time:"2PM", relative: "", savedEvent: "No saved event!"},
-  {time:"3PM", relative: "", savedEvent: "No saved event!"},
-  {time:"4PM", relative: "", savedEvent: "No saved event!"},
-  {time:"5PM", relative: "", savedEvent: "No saved event!"},
+let timeSlots = [
+  {time:"9AM", relative: "", savedEvent: ""},
+  {time:"10AM", relative: "", savedEvent: ""},
+  {time:"11AM", relative: "", savedEvent: ""},
+  {time:"12PM", relative: "", savedEvent: ""},
+  {time:"1PM", relative: "", savedEvent: ""},
+  {time:"2PM", relative: "", savedEvent: ""},
+  {time:"3PM", relative: "", savedEvent: ""},
+  {time:"4PM", relative: "", savedEvent: ""},
+  {time:"5PM", relative: "", savedEvent: ""},
 ];
 
 // Display the current time and date in the jumbo-tron
 currentDayDisplay.text(currentDay);
 
-
-// EXAMPLE: Saving all current settings of timeSlots to localStorage
-localStorage.setItem("allSavedEvents", JSON.stringify(timeSlots));
-
 // Get savedEvents from localStorage (if exists)
 (JSON.parse(localStorage.getItem("allSavedEvents")) !== null) 
  ? timeSlots = JSON.parse(localStorage.getItem("allSavedEvents"))
  : null;
-
- 
 
 // Function to determine what color each hour block should be based on current time
 var determineRelativeTime = function (time) {
@@ -49,7 +43,7 @@ var determineRelativeTime = function (time) {
 };
 
 timeSlots.forEach(function(hourBlock, index) {
-  // variable to set the color of the hourBlock based on determineRelativeTime function
+  // Set the color of the hourBlock based on determineRelativeTime function
   hourBlock.relative = determineRelativeTime(hourBlock.time);
 
   // Create a new row for each timeSlot index to show that time, the color and the savedEvent (if any is saved locally)
@@ -59,9 +53,9 @@ timeSlots.forEach(function(hourBlock, index) {
       <div class="col-sm-2 col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">${hourBlock.time}</div>
         <textarea class="form-control ${hourBlock.relative} description">${hourBlock.savedEvent}</textarea>
         <div class="col-sm-2 col-lg-1 input-group-append">
-        <button class="saveBtn btn-block" type="submit">
-          <i class="far fa-save"></i>
-        </button>
+          <button class="saveBtn btn-block" type="submit">
+            <i class="far fa-save"></i>
+          </button>
       </div>
     </div>
   </div>`;
@@ -70,5 +64,17 @@ timeSlots.forEach(function(hourBlock, index) {
   $(".container").append(hourFormat);
 });
 
+// jQuery Event Listener on 'saveBtn' to set localStorage when button is clicked 
+$(".saveBtn").on("click", function(event) {
+  // Store which hourBlock was clicked
+  let timeBlockID = parseInt($(this).closest(".time-block").attr("id"));
 
+  // Store the user input of the selected block
+  let userInput = $.trim($(this).parent().siblings("textarea").val());
 
+  // Save to savedEvent in timeSlots array
+  timeSlots[timeBlockID].savedEvent = userInput;
+
+  // save to local storage
+  localStorage.setItem("allSavedEvents", JSON.stringify(timeSlots));
+});
